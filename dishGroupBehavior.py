fixed calculation of arena center added IAD calculation added time shifted trajectory control class

dishGroupBehavior.py
@@ -1,45 +1,25 @@
 __author__ = 'jlarsch'
 
-import scipy.io
+
 import matplotlib.pyplot as plt
 import numpy as np
 import Tkinter
 import tkFileDialog
 import joFishHelper
-import matrixUtilities_joh as mu 
-import random
-        
+
+import scipy.io
+      
 avi_path = tkFileDialog.askopenfilename()
 #Tkinter.Tk().withdraw() # Close the root window - not working?
 
 expInfo=joFishHelper.ExperimentMeta(avi_path)
-
 mat=scipy.io.loadmat(expInfo.trajectoryPath)
-trajectories=mat['trajectories']
-maxPixel=np.max(np.max(np.nanmax(trajectories,0)))
-minPixel=np.min(np.min(np.nanmin(trajectories,0)))
-arenaDiameterPx=maxPixel-minPixel
-expInfo.pxPmm=arenaDiameterPx/expInfo.arenaDiameter
-tra=joFishHelper.Pair(trajectories,expInfo)
-plt.plot(trajectories[:,0,0],trajectories[:,0,1],'b.',markersize=1)
+
+        
+tra=joFishHelper.Pair(mat['trajectories'],expInfo)
+
+plt.plot(tra.position[:,0,0],tra.position[:,0,1],'b.',markersize=1)
 plt.show()
 
 #generate shifted control 'mock' pairs
-nRuns=10
-minShift=5*60*expInfo.fps
-
-def shiftPairTrajectory(tra,nRuns,minShift,expInfo):
-    #s=np.shape(tra)
-    sPair=[]
-    #traShiftAll=np.zeros([s[0],s[1],s[2],nRuns])
-    for i in range(10):
-        traShift=tra
-        shiftIndex=int(random.uniform(minShift,tra.shape[0]-minShift))
-        #shift animal 0
-        traShift[:,0,:]=np.roll(tra[:,0,:],shiftIndex,axis=0)
-        #traShiftAll[:,:,:,i]=traShift
-        sPair.append(joFishHelper.Pair(traShift,expInfo))
-    return sPair
-        
-
-#vp=joFishHelper.getVideoProperties(avi_Path)
+test=joFishHelper.shiftedPair(tra,expInfo)

joFishHelper.py
@@ -1,16 +1,15 @@
 import numpy as np
 #import cv2
-import Tkinter
-import tkFileDialog
 import subprocess
 import os
 import matrixUtilities_joh as mu
+import random
 
 class ExperimentMeta(object):
-    def __init__(self,path,arenaDiameter=100):
-        self.arenaDiameter = arenaDiameter
-        self.arenaCenter = [0,0] #assign later
-        self.pxPmm = 0 #assign later
+    def __init__(self,path,arenaDiameter_mm=100):
+        self.arenaDiameter_mm = arenaDiameter_mm
+        self.arenaCenterPx = [0,0] #assign later from class 'Pair'
+        self.pxPmm = 0 #assign later from class 'Pair'
         
         if path.endswith('.avi'):
             self.aviPath = path
@@ -48,10 +47,17 @@ def getVideoProperties(aviPath):
     return decoder_configuration
     
 class Pair(object):
-    def __init__(self, positionPx, videoproperties):
-        self.positionPx=positionPx
+    def __init__(self, trajectories, expInfo):
         
-        self.position=self.positionPx / videoproperties.pxPmm
+
+        self.positionPx=trajectories
+        maxPixel=np.nanmax(self.positionPx,0)
+        minPixel=np.nanmin(self.positionPx,0)
+        arenaDiameterPx=np.mean(maxPixel-minPixel)
+        expInfo.pxPmm=arenaDiameterPx/expInfo.arenaDiameter_mm
+        expInfo.arenaCenterPx=maxPixel-(arenaDiameterPx/2)
+        
+        self.position=(self.positionPx-expInfo.arenaCenterPx) / expInfo.pxPmm
         self.d_position=np.diff(self.position,axis=0)
         self.dd_position=np.diff(self.d_position,axis=0)
         self.speed=np.sqrt(self.d_position[:,:,0]**2 + self.d_position[:,:,1]**2)
@@ -62,20 +68,22 @@ class Pair(object):
         #inter animal distance IAD
         dist=self.position[:,0,:]-self.position[:,1,:]
         self.IAD = np.sqrt(dist[:,0]**2 + dist[:,1]**2)
+        self.IAD_m=np.nanmean(self.IAD)
 
 
+class shiftedPair(object):
+    def __init__(self, tra,expInfo):
+        self.nRuns=10
+        self.minShift=5*60*expInfo.fps
+        self.sPair=[]
+        #generate nRuns instances of Pair class with one animal time shifted against the other
+        for i in range(self.nRuns):
+            traShift=tra.positionPx
+            shiftIndex=int(random.uniform(self.minShift,traShift.shape[0]-self.minShift))
+            #time-rotate animal 0, keep animal 1 as is
+            traShift[:,0,:]=np.roll(traShift[:,0,:],shiftIndex,axis=0)
+            self.sPair.append(Pair(traShift,expInfo))
+        #calculate mean and std IAD for the goup of shifted instances
+        self.spIAD_m = np.nanmean([x.IAD_m for x in self.sPair])
+        self.spIAD_std = np.nanstd([x.IAD_m for x in self.sPair])
        
-    #calculate dependent trajectories, speed, heading etc.    
-        
-
-# while(cap.isOpened()):
-#     ret, frame = cap.read()
-#
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#
-#     cv2.imshow('frame',gray)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# cap.release()
-# cv2.destroyAllWindows()