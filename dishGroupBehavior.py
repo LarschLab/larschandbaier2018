7f3e783f64104c02381fac58531e10eb11eafb48
unknown <jlarsch@O1-298.neuro.mpg.de>
2015-11-10 13:13:56 +0100

    IAD analysis. experiment class for plotting results

--- dishGroupBehavior.py
+++ dishGroupBehavior.py
@@ -1,25 +1,10 @@
 __author__ = 'jlarsch'
 
-
-import matplotlib.pyplot as plt
-import numpy as np
-import Tkinter
+#import Tkinter
 import tkFileDialog
 import joFishHelper
 
-import scipy.io
-      
 avi_path = tkFileDialog.askopenfilename()
 #Tkinter.Tk().withdraw() # Close the root window - not working?
 
-expInfo=joFishHelper.ExperimentMeta(avi_path)
-mat=scipy.io.loadmat(expInfo.trajectoryPath)
-
-        
-tra=joFishHelper.Pair(mat['trajectories'],expInfo)
-
-plt.plot(tra.position[:,0,0],tra.position[:,0,1],'b.',markersize=1)
-plt.show()
-
-#generate shifted control 'mock' pairs
-test=joFishHelper.shiftedPair(tra,expInfo)
+experiment=joFishHelper.experiment(avi_path)
--- joFishHelper.py
+++ joFishHelper.py
@@ -4,13 +4,18 @@ import subprocess
 import os
 import matrixUtilities_joh as mu
 import random
+import scipy.io
+import matplotlib.pyplot as plt
+
 
 class ExperimentMeta(object):
+    #This class collects paths, arena and video parameters
     def __init__(self,path,arenaDiameter_mm=100):
         self.arenaDiameter_mm = arenaDiameter_mm
         self.arenaCenterPx = [0,0] #assign later from class 'Pair'
         self.pxPmm = 0 #assign later from class 'Pair'
         
+        #If a video file name is passed, collect video parameters
         if path.endswith('.avi'):
             self.aviPath = path
             #get video meta data
@@ -23,7 +28,7 @@ class ExperimentMeta(object):
         else:
             self.fps=30
         
-        #concatenate dependent file paths
+        #concatenate dependent file paths (trajectories, pre-analysis)
         head, tail = os.path.split(path)
         head=os.path.normpath(head)
         self.trajectoryPath = os.path.join(head,'trajectories_nogaps.mat')
@@ -46,16 +51,17 @@ def getVideoProperties(aviPath):
     decoder_configuration['fps']=int(float(nominator) / float(denominator))
     return decoder_configuration
     
-class Pair(object):
-    def __init__(self, trajectories, expInfo):
-        
 
+class Pair(object):
+    #Class for trajectories of one pair of animals.
+    #Calculation and storage of dependent time series: speed, heading, etc.
+    def __init__(self, trajectories, expInfo):
         self.positionPx=trajectories
         maxPixel=np.nanmax(self.positionPx,0)
         minPixel=np.nanmin(self.positionPx,0)
         arenaDiameterPx=np.mean(maxPixel-minPixel)
         expInfo.pxPmm=arenaDiameterPx/expInfo.arenaDiameter_mm
-        expInfo.arenaCenterPx=maxPixel-(arenaDiameterPx/2)
+        expInfo.arenaCenterPx=np.mean(maxPixel-(arenaDiameterPx/2),axis=0)
         
         self.position=(self.positionPx-expInfo.arenaCenterPx) / expInfo.pxPmm
         self.d_position=np.diff(self.position,axis=0)
@@ -72,6 +78,7 @@ class Pair(object):
 
 
 class shiftedPair(object):
+    #Class to calculate null hypothesis time series using shifted pairs of animals
     def __init__(self, tra,expInfo):
         self.nRuns=10
         self.minShift=5*60*expInfo.fps
@@ -87,3 +94,53 @@ class shiftedPair(object):
         self.spIAD_m = np.nanmean([x.IAD_m for x in self.sPair])
         self.spIAD_std = np.nanstd([x.IAD_m for x in self.sPair])
        
+class experiment(object):
+    #Class to collect, store and plot data belonging to one experiment
+    def __init__(self,path):
+        self.expInfo=ExperimentMeta(path)
+        mat=scipy.io.loadmat(self.expInfo.trajectoryPath)
+        self.Pair=Pair(mat['trajectories'],self.expInfo)
+        
+        #generate shifted control 'mock' pairs
+        self.sPair=shiftedPair(self.Pair,self.expInfo)        
+        
+        plt.subplot(331)
+        plt.plot(self.Pair.position[:,0,0],self.Pair.position[:,0,1],'b.',markersize=1,alpha=0.1)
+        plt.plot(self.Pair.position[:,1,0],self.Pair.position[:,1,1],'r.',markersize=1,alpha=0.1)
+        plt.xlabel('x [mm]')
+        plt.ylabel('y [mm]')
+        plt.title('raw trajectories')     
+        
+        #plot IAD time series
+        x=np.arange(float(np.shape(self.Pair.IAD)[0]))/(self.expInfo.fps*60)
+        plt.subplot(312)
+        plt.plot(x,self.Pair.IAD,'b.',markersize=0.2)
+        plt.xlim([0, 90]) 
+        plt.xlabel('time [minutes]')
+        plt.ylabel('IAD [mm]')
+        plt.title('Inter Animal Distance over time')
+        
+        plt.subplot(332)
+        #get rid of nan
+        IAD=self.Pair.IAD
+        IAD=IAD[~np.isnan(IAD)]
+        n, bins, patches = plt.hist(IAD, 30, normed=1, histtype='stepfilled') 
+        plt.xlabel('IAD [mm]')
+        plt.ylabel('p')
+        plt.title('IAD')
+        
+        plt.subplot(333)
+        x=[1,2]
+        y=[self.Pair.IAD_m,self.sPair.spIAD_m]
+        yerr=[0,self.sPair.spIAD_std]
+        plt.bar(x, y, yerr=yerr, width=0.5,color='k')
+        lims = plt.ylim()
+        plt.ylim([0, lims[1]]) 
+        plt.xlim([0.5, 3]) 
+        plt.ylabel('[mm]')
+        plt.xticks([1.25,2.25],['raw','shift'])
+        plt.tight_layout()
+        plt.title('mean IAD')
+        plt.show()
+        
+        
